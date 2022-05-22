85fb22595e1035fabec6e650e930a90e9266fa11
Adeyemi987 <tubosun.adeyemi@decagon.dev>
2022-05-22 04:43:56 +0100

    Added CRUD Operations

--- BookLibrary.Infrastructure/Data/DatabaseContexts/BookLibraryDbContext.cs
+++ BookLibrary.Infrastructure/Data/DatabaseContexts/BookLibraryDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookLibrary.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,14 @@ using System.Threading.Tasks;
 
 namespace BookLibrary.Infrastructure.Data.DatabaseContexts
 {
-    public class BookLibraryDbContext 
+    public class BookLibraryDbContext : DbContext
     {
-       
+        public BookLibraryDbContext(DbContextOptions<BookLibraryDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
