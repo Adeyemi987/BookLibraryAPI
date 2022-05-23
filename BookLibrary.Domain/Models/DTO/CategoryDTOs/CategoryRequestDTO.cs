ccf766960229a841f38ac6f17d7e8f1ef1c40180
Adeyemi987 <tubosun.adeyemi@decagon.dev>
2022-05-23 02:09:45 +0100

    DTO, Automapper Implementations and Update Migration Changes

---
+++ BookLibrary.Domain/Models/DTO/CategoryDTOs/CategoryRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Domain.Models.DTO.CategoryDTOs
+{
+    public class CategoryRequestDTO
+    {
+        public string Name { get; set; }
+    }
+}
